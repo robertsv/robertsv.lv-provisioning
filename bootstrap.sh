1d7bc23a8dd7a876e42e7c8bd2683101d23a2dba
roberts_v <Roberts.Vartins@Knowledgeprice.com>
2015-06-26 23:34:48 +0300

    Bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+# install MongoDB
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org=3.0.4
+
+# install nodejs
+sudo apt-get install -y nodejs=0.10.25~dfsg2-2ubuntu1
