e740a40dff10b9017182a35ab92085cdfa7320e2
roberts_v <Roberts.Vartins@Knowledgeprice.com>
2015-08-27 23:27:01 +0300

    OpenJDK 8 and Tomcat 7

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,5 @@
+sudo apt-get update
+
 # install git
 sudo apt-get install -y git=1:1.9.1-1ubuntu0.1
 
@@ -27,3 +29,26 @@ git clone https://github.com/robertsv/robertsv.lv.git
 
 # create server start script
 echo "nohup nodejs /etc/nodejs_server_robertsv_lv/robertsv.lv/index.js > /etc/nodejs_server_robertsv_lv/robertsv_lv_out.log 2>&1 &" > start_robertsv_lv.sh
+
+# install postgress
+
+sudo apt-get install -y postgresql=9.3+154ubuntu1
+sudo apt-get install -y postgresql-client=9.3+154ubuntu1
+
+# set password
+sudo -u postgres psql -U postgres -d postgres -c "alter user postgres with password 'postgres';"
+
+# install java
+
+sudo add-apt-repository ppa:openjdk-r/ppa -y
+sudo apt-get update
+sudo apt-get install openjdk-8-jdk -y
+
+# install tomcat
+
+sudo apt-get install tomcat7 -y
+
+sudo bash -c "echo 'JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> /etc/environment"
+
+# WTF set JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+# sudo nano /etc/default/tomcat7
